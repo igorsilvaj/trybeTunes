3871d493af2a59744740a565e324a93c7df5004a
igorsilvaj <igorjsilvabiz@gmail.com>
2023-03-28 18:11:36 -0300

    feat: add like button to Music cards

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { addSong } from '../services/favoriteSongsAPI';
+import Loading from './Loading';
 
 function MusicCard({ music }) {
+  const { trackId, trackName, previewUrl } = music;
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = async () => {
+    setLoading(true);
+    await addSong(music);
+    setLoading(false);
+  };
+
   return (
     <div>
-      <div>{music.trackName}</div>
-      <audio data-testid="audio-component" src={ music.previewUrl } controls>
+      {loading && <Loading />}
+      <div>{trackName}</div>
+      <audio data-testid="audio-component" src={ previewUrl } controls>
         <track kind="captions" />
         O seu navegador não suporta o elemento.
       </audio>
-      <input type="checkbox" />
+      <input
+        type="checkbox"
+        data-testid={ `checkbox-music-${trackId}` }
+        onChange={ handleChange }
+      />
     </div>
   );
 }
 
 MusicCard.propTypes = {
   music: PropTypes.shape({
+    trackId: PropTypes.number,
     trackName: PropTypes.string,
     previewUrl: PropTypes.string,
   }),
