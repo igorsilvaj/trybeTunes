ea19fe6546924706c23b53d518aac98d144266ef
igorsilvaj <igorjsilvabiz@gmail.com>
2023-03-28 15:26:17 -0300

    feat: add Header navigation links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import { Loading } from '.';
+import logo from '../assets/logo-trybetunes.png';
 
 export default function Header() {
   const firstUpdate = useRef(true);
@@ -8,8 +10,9 @@ export default function Header() {
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    if (firstUpdate) {
+    if (firstUpdate.current) {
       const getUserName = async () => {
+        firstUpdate.current = false;
         setLoading(true);
         const result = await getUser();
         setName(result.name);
@@ -17,20 +20,32 @@ export default function Header() {
       };
       getUserName();
     }
-    firstUpdate.current = false;
   }, []);
 
   return (
     <div data-testid="header-component">
-      {
-        loading
-          ? <Loading />
-          : (
-            <p data-testid="header-user-name">
-              {name}
-            </p>
-          )
-      }
+      { loading && <Loading />}
+      <section>
+        <div>
+          <img src={ logo } alt="logo trybetunes" />
+        </div>
+        <nav>
+          <div>
+            <Link to="/search" data-testid="link-to-search">Pesquisa</Link>
+          </div>
+          <div>
+            <Link to="/favorites" data-testid="link-to-favorites">Favoritas</Link>
+          </div>
+          <div>
+            <Link to="/profile" data-testid="link-to-profile">Perfil</Link>
+          </div>
+        </nav>
+        <div>
+          <p data-testid="header-user-name">
+            {name}
+          </p>
+        </div>
+      </section>
     </div>
   );
 }
