132d7f5ab9914041c57829c5bb4e52609cc0699f
igorsilvaj <igorjsilvabiz@gmail.com>
2023-03-28 14:32:26 -0300

    feat: add login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import logo from "../assets/logo-trybetunes.png";
+import { Loading } from "../components";
+import { createUser } from "../services/userAPI";
 
 export default function Login() {
+  const [name, setName] = useState("");
+  const [isBtnEnterDisabled, setIsBtnEnterDisabled] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const history = useHistory();
+
+  useEffect(() => {
+    name.length >= 3
+      ? setIsBtnEnterDisabled(false)
+      : setIsBtnEnterDisabled(true);
+  }, [name]);
+
   return (
-    <div data-testid="page-login">Login</div>
+    <main data-testid="page-login">
+      <section>
+        <div>
+          {loading && <Loading />}
+          <img src={logo} alt="logo trybetunes" />
+          <input
+            type="text"
+            data-testid="login-name-input"
+            placeholder="qual é o seu nome?"
+            onChange={({ target: { value } }) => setName(value)}
+            value={name}
+          />
+          <button
+            type="submit"
+            data-testid="login-submit-button"
+            disabled={isBtnEnterDisabled}
+            onClick={async () => {
+              setLoading(true);
+              await createUser({ name });
+              setLoading(false);
+              history.push("/search");
+            }}
+          >
+            Entrar
+          </button>
+        </div>
+      </section>
+    </main>
   );
 }
