1da73880711b01ab1b54bc1ea461fcbf69c4ce32
igorsilvaj <igorjsilvabiz@gmail.com>
2023-03-28 21:17:17 -0300

    feat: implement profile page functionality

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
-import { Header } from '../components';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Header, Loading } from '../components';
+import { getUser } from '../services/userAPI';
 
 export default function Profile() {
+  const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    const userInfos = async () => {
+      setLoading(true);
+      setUser(await getUser());
+      setLoading(false);
+    };
+    userInfos();
+  }, []);
+
   return (
     <div data-testid="page-profile">
-      Profile
+      {loading && <Loading />}
       <Header />
+      {Object.values(user).length > 0 && (
+        <section>
+          <img src={ user.image } alt="" data-testid="profile-image" />
+          <h3>Nome</h3>
+          <p>{user.name}</p>
+          <h3>E-mail</h3>
+          <p>{user.email}</p>
+          <h3>Descrição</h3>
+          <p>{user.description}</p>
+          <Link to="/profile/edit">Editar perfil</Link>
+        </section>
+      )}
     </div>
   );
 }
--- src/pages/ProfileEdit.jsx
+++ src/pages/ProfileEdit.jsx
@@ -1,11 +1,92 @@
-import React from 'react';
-import { Header } from '../components';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { Header, Loading } from '../components';
+import { getUser, updateUser } from '../services/userAPI';
 
 export default function ProfileEdit() {
+  const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState({});
+  const [isBtnDisabled, setIsBtnDisabled] = useState(true);
+  const history = useHistory();
+
+  useEffect(() => {
+    const userInfos = async () => {
+      setLoading(true);
+      setUser(await getUser());
+      setLoading(false);
+    };
+    userInfos();
+  }, []);
+
+  useEffect(() => {
+    if (Object.values(user).length > 0) {
+      const { name, email, image, description } = user;
+      const regex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+      const testEmail = regex.test(email);
+      const testLen = [name, image, description].every((e) => e.length > 0);
+      if (testEmail && testLen) return setIsBtnDisabled(false);
+      return setIsBtnDisabled(true);
+    }
+  }, [user]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div data-testid="page-profile-edit">
-      ProfileEdit
+    <div data-testid="page-profile">
+      {loading && <Loading />}
       <Header />
+      {Object.values(user).length > 0 && (
+        <section>
+          <form onSubmit={ handleSubmit } />
+          <img src={ user.image } alt="" data-testid="profile-image" />
+          <input
+            data-testid="edit-input-image"
+            name="userImg"
+            type="text"
+            value={ user.image }
+            onChange={ ({ target: { value } }) => setUser({ ...user, image: value }) }
+          />
+          <h3>Nome</h3>
+          <input
+            data-testid="edit-input-name"
+            name="userName"
+            type="text"
+            value={ user.name }
+            onChange={ ({ target: { value } }) => setUser({ ...user, name: value }) }
+          />
+          <h3>E-mail</h3>
+          <input
+            data-testid="edit-input-email"
+            name="userEmail"
+            type="text"
+            value={ user.email }
+            onChange={ ({ target: { value } }) => setUser({ ...user, email: value }) }
+          />
+          <h3>Descrição</h3>
+          <textarea
+            data-testid="edit-input-description"
+            name="userDescription"
+            type="text"
+            value={ user.description }
+            onChange={
+              ({ target: { value } }) => setUser({ ...user, description: value })
+            }
+          />
+          <button
+            data-testid="edit-button-save"
+            type="submit"
+            onClick={ () => {
+              updateUser(user);
+              history.push('/profile');
+            } }
+            disabled={ isBtnDisabled }
+          >
+            Salvar
+          </button>
+        </section>
+      )}
     </div>
   );
 }
